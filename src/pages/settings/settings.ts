8e723c7d40d6966a254724912497b83f194f5311
Rafael Quintana de Francisco <raquinta@pa.uc3m.es>
2017-05-09 10:57:57 +0200

    Settings are only stored if any field in the form has changed

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -28,13 +28,21 @@ export class SettingsPage {
   }
 
   ionViewDidLeave(){
-    this.connectionInfo.userpass_b64 = btoa(this.connectionInfo.user+":"+this.connectionInfo.pass);
-    // Store connection data on secondary memory
-    this.storage.set('settings_saved', true);
-    this.storage.set('host_dir', this.connectionInfo.host_dir);
-    this.storage.set('listen_port', this.connectionInfo.listen_port);
-    this.storage.set('api_path', this.connectionInfo.api_path);
-    this.storage.set('userpass_b64', this.connectionInfo.userpass_b64);
+    // Settings are only stored if any field in the form has changed
+    if(this.settings.dirty){
+
+      this.connectionInfo.userpass_b64 = btoa(this.connectionInfo.user+":"+this.connectionInfo.pass);
+      // Store connection data on secondary memory
+      this.storage.set('settings_saved', true);
+      this.storage.set('host_dir', this.connectionInfo.host_dir);
+      this.storage.set('listen_port', this.connectionInfo.listen_port);
+      this.storage.set('api_path', this.connectionInfo.api_path);
+      this.storage.set('userpass_b64', this.connectionInfo.userpass_b64);
+
+      // Mark the form as pristine again
+      this.settings.markAsPristine();
+
+    }
   }
 
 }
